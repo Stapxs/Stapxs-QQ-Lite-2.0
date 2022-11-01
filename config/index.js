256c7209f91b7207f09d10dabe640d31ce9f3ca3
Stapxs <1007028430.stapx@gmail.com>
2022-11-01 10:05:36 +0800

    构建目标地址有点问题

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'https://stapxs.github.io/Stapxs-QQ-Lite-2.0/',
 
     /**
      * Source Maps
