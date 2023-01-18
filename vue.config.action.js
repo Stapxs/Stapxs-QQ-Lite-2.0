31e503360e6311befdd04ad8c1429a8e01d7f916
stapxs <1007028430.stapx@gmail.com>
2023-01-18 09:45:27 +0800

    PWA 优化
    :wheelchair: 补充部分遗漏的图标
    :wheelchair: 补全 manifest 文件

--- vue.config.action.js
+++ vue.config.action.js
@@ -17,6 +17,46 @@ module.exports = defineConfig({
         }
     },
     pwa: {
-        name: 'Stapxs QQ Lite'
+        name: 'Stapxs QQ Lite',
+        themeColor: '#606E7A',
+        iconPaths: {
+            faviconSVG: 'img/icons/icon.svg',
+            favicon512: 'img/icons/icon.png',
+            appleTouchIcon: 'img/icons/icon-for-fuck-apple.png',
+            maskIcon: 'img/icons/icon-maskable.png'
+        },
+        manifestOptions: {
+            description: "一个兼容 oicq-http 的非官方网页版 QQ 客户端，使用 Vue 重制的全新版本。",
+            icons: [
+                {
+                    "src": "img/icons/icon.svg",
+                    "sizes": "1080x1080",
+                    "purpose": "any"
+                },
+                {
+                    "src": "img/icons/icon.png",
+                    "type": "image/png",
+                    "sizes": "512x512"
+                }
+                ,
+                {
+                    "src": "img/icons/icon-maskable.png",
+                    "sizes": "1024x1024",
+                    "type": "image/png",
+                    "purpose": "maskable"
+                }
+            ],
+            related_applications: [
+                {
+                  "platform": "play",
+                  "url": "https://play.google.com/store/apps/details?id=com.tencent.mobileqq",
+                  "id": "com.tencent.mobileqq"
+                },
+                {
+                    "platform": "itunes",
+                    "url": "https://apps.apple.com/app/qq/id444934666"
+                }
+            ]
+        }     
     }
 })
